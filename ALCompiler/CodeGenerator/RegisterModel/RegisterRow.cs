c880e3959a44c5107a371934bc86730565363ba1
Gleb Michalev <mixalev702@mail.ru>
2026-01-11 05:40:10 +0300

    CodeGenerator v1

---
+++ ALCompiler/CodeGenerator/RegisterModel/RegisterRow.cs
@@ -0,0 +1,14 @@
+﻿namespace ALCompiler.CodeGenerator.RegisterModel
+{
+    public class RegisterRow
+    {
+        private Dictionary<int, object> _values = new();
+
+        public object? GetValue(int columnIndex) =>
+            _values.TryGetValue(columnIndex, out var value) ? value : null;
+
+        public void SetValue(int columnIndex, object value) =>
+            _values[columnIndex] = value;
+
+    }
+}
